4b90b3c4d11213a0d3d3545f90874a2e77ee343b
omar4321 <akib2000omar@gmail.com>
2021-10-28 11:05:46 +0600

    sodo

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const cors = require('cors');
-
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
